a1ff9d528ec679fee11d27a466b00e77bddbe0ec
aysebzdgn <aysebozdogan989@gmail.com>
2023-05-07 20:58:13 +0300

    function

--- data_extraction.py
+++ data_extraction.py
@@ -2,22 +2,22 @@ import requests
 import json
 
 
+def data_extraction():
+        api_key="ck_2c044dfc1270fe58a29037abcfb4d94685328737"
+        url="https://shop-staging.ilkeczane.com/"
+        response=requests.get(url,
+                                headers={
+                                # "user_agent":"HP",
+                                        "content_type":"application/json"
+                                        },
+                                params={
+                                        "api_key":api_key,
+                                        # "q":price
+                                                }
+                                )
+        return response.text   
 
-api_key="ck_2c044dfc1270fe58a29037abcfb4d94685328737"
-url="https://shop-staging.ilkeczane.com/"
-response=requests.get(url,
-                         headers={
-                        #         "user_agent":"HP",
-                                 "content_type":"application/json"
-                                 },
-                        params={
-                                "api_key":api_key,
-                                # "q":price
-                                        }
-                        )
-        
-
-
+data_extraction()
 print(response.status_code)
 # data=response.text
 data=json.loads(response.text)
