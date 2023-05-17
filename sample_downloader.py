837ab458a8b252ebe5ce12e22e4b3a18329cc364
aysebzdgn <aysebozdogan989@gmail.com>
2023-05-17 12:53:23 +0300

    örnek class

--- sample_downloader.py
+++ sample_downloader.py
@@ -3,12 +3,25 @@ import requests
 
 
 class Downloader:
-    def download(self):
-        response=requests.get(self.url)
+    def __init__(self,url):
+        self.url=url
+        response=requests.get(url)
         with open("download.txt","wb") as f:
             f.write(response.content)
+    
+d1=Downloader( url='https://www.kayserieo.org.tr/dosyalar/image/uresim/havan-5.jpg')
 
 if __name__ == "__main__":
-    d = Downloader(save_to_location="downloads")
-    d.download(url = 'https://www.kayserieo.org.tr/dosyalar/image/uresim/havan-5.jpg')
-    print(f"İndirilen son dosyanın boyutu: {len(d.last_content)}")
+    ...
+    # d = Downloader(save_to_location="downloads")
+    # d.download(url = 'https://www.kayserieo.org.tr/dosyalar/image/uresim/havan-5.jpg')
+    # print(f"İndirilen son dosyanın boyutu: {len(d.last_content)}")
+
+
+
+class kisi:
+    def __init__(self,isim,yas):
+        self.isim=isim
+        self.yas=yas
+
+kisi1=kisi(isim= "ayse", yas=20)
